56e768d2ed3451ae967a4f2b1aaefc0b66c71024
Simamnkele <smambo85@gmail.com>
2024-04-11 13:03:14 +0200

    Pushed regex obfuscating function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""Below module filters logs"""
+import os
+import re
+from typing import List
+
+
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(
+        fields: List[str],
+        redaction: str, message: str, separator: str) -> str:
+    """Function returns obfuscated log message"""
+    extract, replace = (patterns['extract'], patterns['replace'])
+    return (re.sub(extract(fields, separator), replace(redaction), message))
