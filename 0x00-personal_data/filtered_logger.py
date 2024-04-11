ee9027f4875711c323a5919529fd4282cda49d3b
Simamnkele <smambo85@gmail.com>
2024-04-11 17:07:08 +0200

    Fixed pycodestyle errors

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -50,4 +50,3 @@ class RedactingFormatter(logging.Formatter):
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.getMessage(), self.SEPARATOR)
         return (super(RedactingFormatter, self).format(record))
-    
