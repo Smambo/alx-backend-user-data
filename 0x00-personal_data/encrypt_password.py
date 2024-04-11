bd256827276b5ea0a77ee7b8f5ceebadcd1eb350
Simamnkele <smambo85@gmail.com>
2024-04-11 20:03:15 +0200

    Function checks valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 """Module for password encryption"""
+
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
     """Function hashes a password"""
     return (bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()))
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Function checks if given password is hashed"""
+    return (bcrypt.checkpw(password.encode('utf-8'), hashed_password))
