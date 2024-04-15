b275b5353ba09c29e4b3043b6a5e8816af01b9a1
Simamnkele <smambo85@gmail.com>
2024-04-15 17:17:22 +0000

    Fixed pycodestyle error

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -10,7 +10,6 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
