dd9c7feb6c584d2af734c8839e9837c974190e2c
Simamnkele <smambo85@gmail.com>
2024-04-17 15:32:53 +0000

    Implemented method that creates a Session ID for a user_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -8,4 +8,11 @@ from uuid import uuid4
 
 class SessionAuth(Auth):
     """Session Authentication module"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id"""
+        if type(user_id) is str:
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
